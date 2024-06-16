550ca5e2e7df7d47d52cf9973b38e87673d7430c
Nick <nick@odnari.com>
2024-06-16 21:55:01 +0300

    fix: private api warning

--- lib/views/signup.dart
+++ lib/views/signup.dart
@@ -9,7 +9,7 @@ class AuthPage extends StatefulWidget {
   const AuthPage({super.key});
 
   @override
-  _AuthPageState createState() => _AuthPageState();
+  State<AuthPage> createState() => _AuthPageState();
 }
 
 class _AuthPageState extends State<AuthPage> {
